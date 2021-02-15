1c8880085c6f589629e9339698ff2112a08be143
SunnySparks <josediazt6@outlook.com>
2021-02-15 14:16:13 -0600

    create post model, post controller and setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :posts
+  resources :posts, path: 'auth', path_names: [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
